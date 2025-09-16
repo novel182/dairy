cc2d77feeb60f6c1610a4a758bd831b65fd43574
Novel Poudel <poudelnovel@gmail.com>
2025-09-16 14:34:22 -0400

    zoom on hover

--- src/Components/ProductShowcase/ProductCard.tsx
+++ src/Components/ProductShowcase/ProductCard.tsx
@@ -6,7 +6,9 @@ type ProductCardProps = {
 const ProductCard = ({title, description}: ProductCardProps) => {
     return (
         <div className="border border-slate-100 shadow-md hover:shadow-lg rounded-lg px-4 py-10 max-w-[350px] bg-yellow-50">
-            <img src="placeholder.jpg" className="rounded-lg"/>
+            <div className="overflow-hidden rounded-lg">
+                <img src="placeholder.jpg" className="hover:scale-105"/>    
+            </div>
             <h2 className="text-3xl mt-3">{title}</h2>
             <p>{description}</p>
         </div>
