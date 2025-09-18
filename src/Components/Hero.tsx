ca2ef11f8540d4bbb19530c964568a6aab2f692c
Novel Poudel <poudelnovel@gmail.com>
2025-09-17 21:43:54 -0400

    starting order request

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,14 +1,14 @@
 const Hero = () => {
-    const headline = 'Fresh Dairy, Straight from the Farm'
+    const headline = 'Freshness from Farm to Dairy'
     const ctaText = 'Request an order'
 
     return (
         <section className="h-screen flex items-center justify-center">
             <div className="absolute inset-0 bg-[url(hero2.jpg)] bg-no-repeat bg-cover bg-center">
-                <div className="absolute inset-0 bg-black/40"/>
+                <div className="absolute inset-0 bg-black/20"/>
             </div>
             <div className="items-center text-center margin-auto z-2">
-                <h1>{headline}</h1>
+                <h1 className="text-5xl font-bold">{headline}</h1>
                 <button className="mt-7">{ctaText}</button>
             </div>
         </section>
