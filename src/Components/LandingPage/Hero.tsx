95a6d021f7f2f379c0101076b304a80aa878eab8
Novel Poudel <poudelnovel@gmail.com>
2025-12-04 09:04:23 -0500

    can serve static sites

--- src/Components/LandingPage/Hero.tsx
+++ src/Components/LandingPage/Hero.tsx
@@ -3,14 +3,14 @@ import { useNavigate } from "react-router-dom"
 import { primary } from "utils/colors"
 
 const Hero : React.FC = () => {
-    const headline = 'Freshness from Farm to Dairy'
+    const headline = 'Freshness from Farm to Dairy1'
     const ctaText = 'Request an order'
 
     const navigate = useNavigate()
 
     return (
         <section className="h-screen flex items-center justify-center text-white">
-            <div className="absolute inset-0 bg-[url(hero2.jpg)] bg-no-repeat bg-cover bg-center">
+            <div className="absolute inset-0 bg-no-repeat bg-cover bg-center" style={{backgroundImage:"url('hero.jpg')"}}>
                 <div className="absolute inset-0 bg-black/20"/>
             </div>
             <div className="items-center text-center margin-auto z-2">
